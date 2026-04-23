9cd7fa22ea5cccf1ed8c9656f2cb6b2c0c215884
Anagha Anilkumar <mdl24cs032@mec.ac.in>
2026-04-23 20:19:27 +0530

    added grade function and bar graph

--- test.py
+++ test.py
@@ -1,13 +1,33 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data=pd.read_csv('data.csv')
 print("Full data:\n",data)
-print("\nAverage marks:",data['marks'].mean())
+data['result']=data['marks']>40
 print("\nTop student:\n",data.loc[data['marks'].idxmax()])
 print("\nMarks >80:\n",data[data['marks']>80])
-data=data.sort_values('marks',ascending=False)
-print("\nData sorted by marks:\n",data)
 print("Highest marks:",data['marks'].max())
 print("Lowest marks:",data['marks'].min())
 print("Average marks:",data['marks'].mean())
-print(data.groupby('name')['marks'].mean())
+data=data.sort_values('marks',ascending=False)
+print("\nData sorted by marks:\n",data)
+
+print(data.groupby('result')['marks'].mean())
+print(data['result'].value_counts())
+def grade(marks):
+    if marks >= 90:
+        return 'A'
+    elif marks >= 75:
+        return 'B'
+    elif marks >= 50:
+        return 'C'
+    else:
+        return 'Fail'
+
+data['grade']=data['marks'].apply(grade)
+print("\nStudents per grade:")
+print(data['grade'].value_counts())
+print(data.groupby('grade')['marks'].mean())
+data['marks'].plot(kind='bar')
+plt.show()
+
